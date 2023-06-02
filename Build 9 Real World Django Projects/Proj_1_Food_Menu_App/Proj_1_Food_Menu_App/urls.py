5a0410ea83d82816a97f36a199dd9d781f943a42
Stas <113462863+shamansk1y@users.noreply.github.com>
2023-06-02 13:37:18 +0200

    Add files via upload

--- Build 9 Real World Django Projects/Proj_1_Food_Menu_App/Proj_1_Food_Menu_App/urls.py
+++ Build 9 Real World Django Projects/Proj_1_Food_Menu_App/Proj_1_Food_Menu_App/urls.py
@@ -14,9 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
+from account import views as user_views
+from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from account.views import registration_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('food/',include('food.urls')),
+    path('register/', registration_view, name='registration_view'),
+    path('login/', authentication_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
+    path('accounts/profile/', user_views.profilepage,name='profile'),
 ]
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
